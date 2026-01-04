9f94f15e6b3e60210168d3e845644f5c4a069a97
romadaje14-create <romadaje14@gmail.com>
2026-01-04 01:10:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,11 +8,8 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['390371717717', 'nobody|'],
-  ['393518398856', 'nobody|'],
-  ['393755775755', 'nobody|'],
-  ['393755575577', 'nobody|'],
-  ['33760536110', 'nobody|'],
+  ['393780422257', 'nobody|'],
+  
 ]
 global.suittag = ['390371717717'] 
 global.prems = ['390371717717'] 
